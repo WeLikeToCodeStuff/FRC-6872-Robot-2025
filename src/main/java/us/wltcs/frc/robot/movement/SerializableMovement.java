92768a500924d63362a094f7021308c67e554914
Adrian T <adrian.trifonov@gmail.com>
2025-01-15 18:58:46 -0500

    modified serializable movement to work with all buttons too, universal support for all controllers

--- src/main/java/us/wltcs/frc/robot/movement/SerializableMovement.java
+++ src/main/java/us/wltcs/frc/robot/movement/SerializableMovement.java
@@ -1,11 +1,32 @@
 package us.wltcs.frc.robot.movement;
 
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj.Joystick;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Locked;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
+@Data
 public class SerializableMovement {
-    public float x;
-    public float y;
-    public float z;
-    public final boolean isButton1Held;
+    private final double x, y, z;
+    private final Map<Integer, Boolean> buttons = new HashMap<>();  
+
+    public SerializableMovement(Joystick joystick) {
+        this.x = joystick.getX();
+        this.y = joystick.getZ();
+        this.z = joystick.getZ();
+        for (int i = 1; i <= joystick.getButtonCount(); i++) {
+            try {
+                this.buttons.put(i, joystick.getRawButton(i));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
