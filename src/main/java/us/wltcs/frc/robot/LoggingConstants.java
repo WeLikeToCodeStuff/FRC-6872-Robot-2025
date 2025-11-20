120d0b99502be8efd3d7c04128c0ecd1d761f30f
Bonzai-Dev <2690360@CSMV.QC.CA>
2025-11-20 14:03:49 -0500

    Refactored codebase
    Committer: Bonzai-Dev

--- src/main/java/us/wltcs/frc/robot/LoggingConstants.java
+++
@@ -1,9 +0,0 @@
-package us.wltcs.frc.robot;
-
-import us.wltcs.frc.robot.util.logging.Logger;
-
-public class LoggingConstants {
-    public static final Logger mainLogger = new Logger("Main");
-    public static final Logger movementLogger = new Logger("Movement");
-    public static final Logger motorLogger = new Logger("Motor");
-}
--- src/main/java/us/wltcs/frc/robot/record/reader/RecordReader.java
+++
@@ -1,7 +0,0 @@
-package us.wltcs.frc.robot.record.reader;
-
-public interface RecordReader {
-
-    void execute(String[] parameters);
-
-}
