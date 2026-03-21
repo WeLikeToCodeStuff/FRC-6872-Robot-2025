e7e76da0ad5e870e36401474d16d57cf819e36f5
Adrian T <adrian.trifonov@gmail.com>
2026-03-21 09:24:44 -0400

    changed dashboard from Runnable to Callable

--- src/main/java/us/wltcs/frc/core/ui/Dashboard.java
+++ src/main/java/us/wltcs/frc/core/ui/Dashboard.java
@@ -2,6 +2,7 @@ package us.wltcs.frc.core.ui;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
 import edu.wpi.first.networktables.GenericEntry;
@@ -15,7 +16,7 @@ public class Dashboard {
   private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
   private final NetworkTable table = inst.getTable("6872 Robot");
   private final Map<String, NetworkTableEntry> entries;
-  private final Map<String, Runnable> callbackMap;
+  private final Map<String, Callable> callbackMap;
 
   public Dashboard() {
     this.entries = new HashMap<>();
@@ -23,17 +24,15 @@ public class Dashboard {
   }
 
   public void update() {
-    callbackMap.forEach((key, runnable) -> {
-      setValue(entries.get(key), runnable);
+    callbackMap.forEach((key, callback) -> {
+      setValue(entries.get(key), callback);
     });
   }
 
   public <T> void addEntry(String key, Supplier<T> supplier) {
     NetworkTableEntry entry = table.getEntry(key);
     entries.put(key, entry);
-    callbackMap.put(entry.getTopic().getName(), () -> {
-      setValue(entry, supplier.get());
-    });
+    callbackMap.put(entry.getTopic().getName(), supplier::get);
   }
 
   private <T> void setValue(NetworkTableEntry entry, T value) {
