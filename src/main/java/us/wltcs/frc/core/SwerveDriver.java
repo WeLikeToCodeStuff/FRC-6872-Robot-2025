9dfa77fe7a1af687b49c43c5f288dd28ba62213d
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2026-04-03 12:08:29 -0400

    Fix turning

--- src/main/java/us/wltcs/frc/core/SwerveDriver.java
+++ src/main/java/us/wltcs/frc/core/SwerveDriver.java
@@ -3,8 +3,11 @@ package us.wltcs.frc.core;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.SwerveController;
 import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import us.wltcs.frc.core.logging.Context;
@@ -13,48 +16,61 @@ import us.wltcs.frc.core.math.vector2.Vector2d;
 import java.io.File;
 import java.io.IOException;
 
+import static edu.wpi.first.wpilibj.RobotBase.isSimulation;
+
 public class SwerveDriver {
-  private final float driveSpeed;
+  private final float maxSpeed;
   private final float turnSpeed;
 
   private Vector2d position;
   private double rotationRadians;
 
   private final SwerveDrive swerveDriver;
+  private final Robot robot;
 
-  public SwerveDriver(float driveSpeed, float turnSpeed, Vector2d position) {
-    this.driveSpeed = driveSpeed;
+  public SwerveDriver(float maxSpeed, float turnSpeed, Vector2d position, Robot robot) {
+    this.maxSpeed = maxSpeed;
     this.turnSpeed = turnSpeed;
+    this.robot = robot;
 
     this.position = position;
 
-    SwerveDriveTelemetry.verbosity = SwerveDriveTelemetry.TelemetryVerbosity.HIGH;
+    if (isSimulation())
+      SwerveDriveTelemetry.verbosity = SwerveDriveTelemetry.TelemetryVerbosity.HIGH;
+
     File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve/sparkmax");
     try {
-      this.swerveDriver = new SwerveParser(swerveJsonDirectory).createSwerveDrive(this.driveSpeed);
+      this.swerveDriver = new SwerveParser(swerveJsonDirectory).createSwerveDrive(
+        this.maxSpeed,
+        new Pose2d(new Translation2d(position.x, position.y), new Rotation2d())
+      );
+      System.out.println(swerveDriver.getPose());
     } catch (IOException e) {
       Context.movement.logError("Failed to initialize swerve modules");
       throw new RuntimeException(e);
     }
 
-    swerveDriver.setCosineCompensator(false);
-    swerveDriver.setHeadingCorrection(false);
+//    swerveDriver.setCosineCompensator(true);
+//    swerveDriver.setHeadingCorrection(false);
     swerveDriver.zeroGyro();
-
-    swerveDriver.resetOdometry(new Pose2d(new Translation2d(position.x, position.y), new Rotation2d()));
   }
 
   // fieldRelative defines whether the forward direction to move at is either the robot's forward direction or the field's
   public void drive(Vector2d movementInput, Vector2d rotationInput, boolean fieldRelative) {
+    movementInput = movementInput.normalized().times(swerveDriver.getMaximumChassisVelocity());
+    rotationInput = rotationInput.normalized();
+
+    Translation2d direction = new Translation2d(-movementInput.y, -movementInput.x);
+    double rotation = -rotationInput.x * swerveDriver.getMaximumChassisAngularVelocity();
+    System.out.println(movementInput.toString());
+
     swerveDriver.drive(
-      new Translation2d(-movementInput.y * driveSpeed, -movementInput.x * driveSpeed),
-      -rotationInput.x * turnSpeed,
+      direction, rotation,
       fieldRelative,
       false
     );
 
     position = new Vector2d(swerveDriver.getPose().getX(), swerveDriver.getPose().getY());
     rotationRadians = swerveDriver.getPose().getRotation().getRadians();
-    System.out.println(rotationRadians);
   }
 }
